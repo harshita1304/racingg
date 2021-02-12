9f8314f26a1a854b5d3318f27180ad07045fa723
harshita1304 <75071185+harshita1304@users.noreply.github.com>
2021-02-12 16:00:12 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,33 @@
+class Form{
+    constructor(){
+        this.input=createInput("name");
+        this.button=createButton('play');
+        this.greeting=createElement('h3');
+    }
+    hide(){
+        this.input.hide();
+        this.button.hide();
+        this.greeting.hide();
+        console.log("blah");
+    }
+    display(){
+        var title=createElement('h2');
+        title.html("car racing game");
+        title.position(130,0);
+        
+        this.input.position(130,160);
+        this.button.position(130,200);
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+
+            player.name=this.input.value();
+            playerCount+=1;
+            player.update(name);
+            player.updateCount(playerCount);
+            
+            this.greeting.html("hello"+player.name);
+            this.greeting.position(130,250);
+        })
+    }
+}
